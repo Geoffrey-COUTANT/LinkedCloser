2c8a86dd2e43b508830d4ca310ea52ca1898dbfe
Geoffrey <geoffrey22.coutant@outlook.fr>
2024-04-23 10:32:43 +0200

    add images and continue onboarding 3

--- src/components/QuizOnboarding/QuizOnboarding.jsx
+++ src/components/QuizOnboarding/QuizOnboarding.jsx
@@ -45,26 +45,42 @@ function QuizOnboardingStep2({onNext, onPrev}) {
 
 function QuizOnboardingStep3({onNext, onPrev}) {
     return (
-        <div className='flex justify-items-center'>
-            <div className='bg-cyan-900/30 rounded-lg border-4 border-gray-500/25 backdrop-blur-lg py-16'>
-                <div className='mb-2.5 mx-10'>
-                    <div className='flex flex-grow '>
-                        <h1 className='text-4xl ml-3.5 mr-96 text-white font-bold'>PARTIE 1 : CRÉER LE CONTACT</h1>
-                        <Chronometre />
-                    </div>
-                    <div className='flex flex-col bg-blue-950/20 my-72 items-center'>
-                        <div className='max-w-xl rounded-lg text-white'>
-                            <div className='mx-3.5'>
-                                <h1 className='text-2xl font-bold'>Bonjour John ! Comment allez-vous ?</h1>
-                                <h2 className='text-xl '>D'où est-ce que vous m'appelez ?</h2>
+        <div className='flex flex-col'>
+            <div className='flex justify-items-center'>
+                <div className='bg-gray-400/30 rounded-lg border-4 border-gray-500/25 backdrop-blur-sm pt-14'>
+                    <div className='mb-2.5 mx-10'>
+                        <div className='flex flex-grow '>
+                            <h1 className='text-4xl ml-3.5 mr-96 text-white font-bold'>PARTIE 1 : CRÉER LE CONTACT</h1>
+                            <Chronometre />
+                        </div>
+                        <div className='flex flex-col bg-gray-500/20 py-52 mt-16 rounded-lg items-center'>
+                            <div className='text-white'>
+                                <div className='mx-3.5'>
+                                    <h1 className='text-3xl font-bold'>Bonjour John ! Comment allez-vous ?</h1>
+                                    <h2 className='text-2xl mt-7'>D'où est-ce que vous m'appelez ?</h2>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='mt-10 mb-8 grid grid-cols-6 gap-2'>
+                            <div className='flex flex-col col-start-1 col-end-3'>
+                                <div className='flex items-center bg-gray-600 rounded-full w-80'>
+                                    <img className='h-12 w-20 my-5 ml-1' src={require('../img/intonation-emoji/stars.png')}
+                                         alt="logo"/>Prenez une intonation "enthousiaste"
+                                </div>
+                            </div>
+                            <div className='col-end-11 col-span-2'>
+                                <button onClick={onPrev}
+                                        className='flex bg-gray-400 hover:bg-gray-500 text-white font-bold py-2 px-4 rounded-full'>
+                                    <img className='h-10 w-10 my-5 mx-3' src={require('../img/play.png')} alt="logo"/>
+                                </button>
                             </div>
                         </div>
                     </div>
-                    <div className='flex flex-col items-end mt-10'>
-                        <button onClick={onPrev}
-                                className='flex bg-gray-400 hover:bg-gray-500 text-white font-bold py-2 px-4 rounded-full'><img className='h-10 w-10 my-5 mx-3' src={require('../img/play.png')} alt="logo"/>
-                        </button>
-                    </div>
+                </div>
+            </div>
+            <div>
+                <div>
+
                 </div>
             </div>
         </div>
